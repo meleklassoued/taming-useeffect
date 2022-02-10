0390e445ec775dd79f58c73c3493c9eeb4dec07c
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-02-10 14:21:19 +0100

    fixing our dependecies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { useFetch } from "./useFetch";
+import { useState } from "react";
 function App() {
+  const [url, setUrl] = useState(null);
   const { data } = useFetch({
-    url: "/melek.json",
+    url,
   });
+
   return (
     <div className='App'>
       <header className='App-header'>hello {JSON.stringify(data)}</header>
+      <button onClick={() => setUrl("./melek.json")}> click</button>
+      <button onClick={() => setUrl("./jawher.json")}> click mara okhra</button>
     </div>
   );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const useFetch = (options) => {
   const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log("useeffect use fetch"); 
+
+    if (options.url) {
+      fetch(options.url)
+        .then((response) => response.json())
+        .then((json) => setData(json));
+    }
+  }, [options.url]);
   return {
     data,
   };
